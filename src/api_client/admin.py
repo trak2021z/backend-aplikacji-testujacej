e6b3c367f7fbfc1783ef086b9dea78e98a734906
unknown <michal.maslanka.7@gmail.com>
2020-11-01 15:03:46 +0100

    added model and migration

--- src/api_client/admin.py
+++ src/api_client/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Test)
+admin.site.register(TestCall)
+admin.site.register(Result)
+admin.site.register(Endpoint)
+admin.site.register(TestEndpoint)
--- src/api_client/models.py
+++ src/api_client/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Test(models.Model):
+    name = models.CharField(max_length=50, default="")
+    description = models.CharField(max_length=150, default="")
+
+class TestCall(models.Model):
+    test = models.ForeignKey('Test', on_delete=models.CASCADE, related_name='calls')
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(auto_now=True, null=True, blank=True)
+    num_users = models.PositiveIntegerField(default=0)
+    is_finished = models.BooleanField(default=False)
+
+class Result(models.Model):
+    test_call = models.ForeignKey('TestCall', on_delete=models.CASCADE, related_name='results')
+    results = models.TextField()
+
+class Endpoint(models.Model):
+    url = models.CharField(max_length=50, default="")
+    name = models.CharField(max_length=50, default="")
+    request = models.TextField()
+
+class TestEndpoint(models.Model):
+    test = models.ForeignKey('Test', on_delete=models.CASCADE, related_name='test_endpoints')
+    endpoint = models.ForeignKey('Endpoint', on_delete=models.CASCADE, related_name='test_endpoints')
+    order = models.PositiveIntegerField(default=0)
