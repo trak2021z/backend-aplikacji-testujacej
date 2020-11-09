75319711f9ae64978339ad663bb5e307031377f8
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2020-11-09 22:53:46 +0100

    Update settings.py

--- src/testing_app/settings.py
+++ src/testing_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'FRONT',
+]
