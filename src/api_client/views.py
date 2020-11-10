c6b7adc55a9f065b6b73f326a86efec8abb6bd62
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2020-11-10 19:26:49 +0100

    Pep8 Corrections and header requirement

--- src/api_client/views.py
+++ src/api_client/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from drf_yasg2.openapi import Parameter
 from drf_yasg2.utils import swagger_auto_schema, is_list_view
 from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
@@ -20,7 +21,8 @@ class TestView(APIView):
     testSerializer = TestSerializer
     testDetailsSerializer = TestDetailsSerializer
 
-    @swagger_auto_schema(responses={200: testSerializer()})
+    @swagger_auto_schema(responses={200: testSerializer()},
+                         manual_parameters=[Parameter(name="FRONT", in_='header', type='str')])
     def get(self, request, pk=None, format=None):
         if pk:
             serializer = self.get_single(request, pk, format).data
@@ -33,7 +35,8 @@ class TestView(APIView):
         ids = list(TestEndpoint.objects.values().filter(test_id=pk).values_list('endpoint_id', flat=True))
         endpoints = list(Endpoint.objects.values().filter(id__in=ids))
         for endpoint in endpoints:
-            endpoint['order'] = TestEndpoint.objects.filter(test_id=pk).filter(endpoint_id=endpoint['id']).values('order').values_list('order', flat=True)
+            endpoint['order'] = TestEndpoint.objects.filter(test_id=pk).filter(endpoint_id=endpoint['id']).values(
+                'order').values_list('order', flat=True)
         return self.testDetailsSerializer(tests, context={'endpoints': endpoints})
 
     def get_many(self, request, format=None):
@@ -41,14 +44,15 @@ class TestView(APIView):
         tests = list(Test.objects.values())
         for test in tests:
             endpoints_count = TestEndpoint.objects.filter(test_id=test['id']).count()
-            result.append(self.testSerializer(test, context={'endpoints_count': endpoints_count}).data)   
+            result.append(self.testSerializer(test, context={'endpoints_count': endpoints_count}).data)
         return result
 
 
 class ResultView(APIView):
     testResultsSerializer = TestResultsSerializer
 
-    @swagger_auto_schema(responses={200: testResultsSerializer()})
+    @swagger_auto_schema(responses={200: testResultsSerializer()},
+                         manual_parameters=[Parameter(name="FRONT", in_='header', type='str')])
     def get(self, request, pk=None, format=None):
         if pk:
             serializer = self.get_single(request, pk, format).data
@@ -65,8 +69,8 @@ class ResultView(APIView):
 
         for testCall in testCalls:
             results = list(Result.objects.values('results')
-                                       .filter(test_call_id=testCall['id'])
-                                       .values_list('results', flat=True))
+                           .filter(test_call_id=testCall['id'])
+                           .values_list('results', flat=True))
             json_results = []
             for result_str in results:
                 json_results.append(json.loads(result_str))
@@ -91,10 +95,13 @@ class ResultView(APIView):
                 testCall['results'] = json_results
             result.append(self.testResultsSerializer(test, context={'testCalls': testCalls}).data)
         return result
-            
+
+
 class TestCallView(APIView):
     serializer_class = TestCallSerializer
 
+    @swagger_auto_schema(responses={200: serializer_class()},
+                         manual_parameters=[Parameter(name="FRONT", in_='header', type='str')])
     def get(self, request, pk=None, format=None):
         if pk:
             try:
@@ -107,24 +114,29 @@ class TestCallView(APIView):
             test_calls = TestCall.objects.all()
             serializer = self.serializer_class(test_calls, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
-            serializer = TestCallInputSerializer(data=request.data, fields=('test', 'num_users', 'max_calls'))
-            if serializer.is_valid():
-                test_id = serializer.validated_data['test']
-                try:
-                    test_call = TestCall.objects.create(
-                        test=test_id,
-                        num_users=serializer.validated_data["num_users"],
-                        max_calls=serializer.validated_data["max_calls"]
-                    )
-                    save_serializer = TestCallSerializer(test_call)
-                    return Response(save_serializer.data, status=status.HTTP_201_CREATED)
-                except Exception as e:
-                    return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_400_BAD_REQUEST)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = TestCallInputSerializer(data=request.data, fields=('test', 'num_users', 'max_calls'))
+        if serializer.is_valid():
+            test_id = serializer.validated_data['test']
+            try:
+                test_call = TestCall.objects.create(
+                    test=test_id,
+                    num_users=serializer.validated_data["num_users"],
+                    max_calls=serializer.validated_data["max_calls"]
+                )
+                save_serializer = TestCallSerializer(test_call)
+                return Response(save_serializer.data, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class TestCallDetailsView(APIView):
     serializer_class = TestCallDetailsSerializer
 
+    @swagger_auto_schema(responses={200: serializer_class()},
+                         manual_parameters=[Parameter(name="FRONT", in_='header', type='str')])
     def get(self, request, pk=None, format=None):
         if pk:
             try:
@@ -142,8 +154,12 @@ class TestCallDetailsView(APIView):
         else:
             return Response({'error': 'No pk specified'}, status=status.HTTP_404_NOT_FOUND)
 
+
 class TestCallByDateView(APIView):
     serializer_class = TestCallSerializer
+
+    @swagger_auto_schema(responses={200: serializer_class()},
+                         manual_parameters=[Parameter(name="FRONT", in_='header', type='str')])
     def get(self, request, test_date=None, format=None):
         if test_date:
             try:
@@ -154,7 +170,8 @@ class TestCallByDateView(APIView):
             date = datetime.now()
         data = self.get_many(request, date, format)
         return Response(data, status=status.HTTP_200_OK)
-    def get_many(self, request,  date, format=None):
+    
+    def get_many(self, request, date, format=None):
         test_calls = TestCall.objects.filter(start_date__date=date.date())
         serializer = self.serializer_class(test_calls, many=True)
         return serializer.data
