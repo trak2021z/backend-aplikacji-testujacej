200c52d99254a83868d94f782da8fdb5cc7273a2
mzyga4 <37455997+mzyga4@users.noreply.github.com>
2020-11-14 16:23:02 +0100

    seed_test update
    added tests from the document; wip

--- src/api_client/management/commands/seed_test.py
+++ src/api_client/management/commands/seed_test.py
@@ -14,9 +14,9 @@ class Command(BaseCommand):
         company_detail = Endpoint.objects.create(url='company/<company_id>', name="Szczegóły dotyczące firmy", request="")
         stocks = Endpoint.objects.create(url='stocks/', name="Lista wszystkich dostępnych zasobów", request="")
         stock_detail = Endpoint.objects.create(url='stock/<stock_id>', name="Szczegóły pojedynczego zasobu", request="")
-        stock_buy = Endpoint.objects.create(url='stock/<stock_id>/buy/', name="Szczegóły pojedynczego zasobu",
+        stock_buy = Endpoint.objects.create(url='stock/<stock_id>/buy/', name="Kupno pojedynczego zasobu",
                                 request='{"quantity": 0}')
-        stock_create = Endpoint.objects.create(url='stock/<stock_id>/sell/', name="Szczegóły pojedynczego zasobu",
+        stock_create = Endpoint.objects.create(url='stock/<stock_id>/sell/', name="Utworzenie pojedynczego zasobu",
                                 request='{"quantity": 0}')
         transactions_stock = Endpoint.objects.create(url='transaction/<stock_id>',
                                 name="Lista wszystkich zleceń kupna i sprzedaży dla danego zasobu", request="")
@@ -27,14 +27,66 @@ class Command(BaseCommand):
                                 request="")
         history = Endpoint.objects.create(url='user/history/', name="Lista zrealizowanych transakcji danego użytkownika",
                                 request="")
+        buy_offer = Endpoint.objects.create(url='buyoffer/', name="Utworzenie oferty kupna",
+                                request="")
+        sell_offer = Endpoint.objects.create(url='selloffer/', name="Utworzenie oferty sprzedaży",
+                                request="")
 
         endpoints = Endpoint.objects.all()
         print(endpoints)
 
-        example_test = Test.objects.create(name= "Example Test", description="Example Test", class_name="ExampleTest")
-        TestEndpoint.objects.create(test=example_test, endpoint=log_in, order=0)
-        TestEndpoint.objects.create(test=example_test, endpoint=transactions, order=1)
-        TestEndpoint.objects.create(test=example_test, endpoint=transactions, order=2)
-        TestEndpoint.objects.create(test=example_test, endpoint=transactions, order=3)
+        test_1 = Test.objects.create(name= "Kupuj kolejne, dopóki są fundusze", 
+                                    description="Kupno kolejnych akcji z listy do końca funduszy lub akcji", class_name="Test1")
+        TestEndpoint.objects.create(test=test_1, endpoint=registration, order=0)
+        TestEndpoint.objects.create(test=test_1, endpoint=log_in, order=1)
+        TestEndpoint.objects.create(test=test_1, endpoint=stocks, order=2)
+        TestEndpoint.objects.create(test=test_1, endpoint=wallet, order=3)
+        TestEndpoint.objects.create(test=test_1, endpoint=stock_buy, order=4)
+
+        test_2 = Test.objects.create(name= "Oferty kupna do oporu", 
+                                    description="Wystawiaj oferty kupna do końca akcji", class_name="Test2")
+        TestEndpoint.objects.create(test=test_2, endpoint=registration, order=0)
+        TestEndpoint.objects.create(test=test_2, endpoint=log_in, order=1)
+        TestEndpoint.objects.create(test=test_2, endpoint=stocks, order=2)
+        TestEndpoint.objects.create(test=test_2, endpoint=buy_offer, order=3)
+        TestEndpoint.objects.create(test=test_2, endpoint=offers, order=4)
+
+        test_3 = Test.objects.create(name= "Kupuj do oporu i wystawiaj oferty sprzedaży", 
+                                    description="Kupno do oporu, wystawiaj oferty sprzedaży do końca akcji", class_name="Test3")
+        TestEndpoint.objects.create(test=test_3, endpoint=registration, order=0)
+        TestEndpoint.objects.create(test=test_3, endpoint=log_in, order=1)
+        TestEndpoint.objects.create(test=test_3, endpoint=stocks, order=2)
+        TestEndpoint.objects.create(test=test_3, endpoint=wallet, order=3)
+        TestEndpoint.objects.create(test=test_3, endpoint=stock_buy, order=4)
+        TestEndpoint.objects.create(test=test_3, endpoint=offers, order=5
+        TestEndpoint.objects.create(test=test_3, endpoint=history, order=6)
+
+        test_4 = Test.objects.create(name= "Kupuj i sprzedawaj", 
+                                    description="Kupno do oporu, wystawiaj oferty do połowy posiadanych akcji", class_name="Test4")
+        TestEndpoint.objects.create(test=test_4, endpoint=registration, order=0)
+        TestEndpoint.objects.create(test=test_4, endpoint=log_in, order=1)
+        TestEndpoint.objects.create(test=test_4, endpoint=stocks, order=2)
+        TestEndpoint.objects.create(test=test_4, endpoint=wallet, order=3)
+        TestEndpoint.objects.create(test=test_4, endpoint=stock_buy, order=4)
+        TestEndpoint.objects.create(test=test_4, endpoint=sell_offer, order=5)
+        TestEndpoint.objects.create(test=test_4, endpoint=offers, order=6)
+
+        test_5 = Test.objects.create(name= "Kupuj do oporu", 
+                                    description="Kupno danej akcji do końca akcji lub funduszy", class_name="Test5")
+        TestEndpoint.objects.create(test=test_5, endpoint=registration, order=0)
+        TestEndpoint.objects.create(test=test_5, endpoint=log_in, order=1)
+        TestEndpoint.objects.create(test=test_5, endpoint=stocks, order=2)
+        TestEndpoint.objects.create(test=test_5, endpoint=wallet, order=3)
+        TestEndpoint.objects.create(test=test_5, endpoint=stock_buy, order=4)
+
+        test_6 = Test.objects.create(name= "Wyświetl wszystko", 
+                                    description="Wyświetlanie kolejno wszystkich list i historii", class_name="Test6")
+        TestEndpoint.objects.create(test=test_6, endpoint=registration, order=0)
+        TestEndpoint.objects.create(test=test_6, endpoint=log_in, order=1)
+        TestEndpoint.objects.create(test=test_6, endpoint=stocks, order=2)
+        TestEndpoint.objects.create(test=test_6, endpoint=user_stocks, order=3)
+        TestEndpoint.objects.create(test=test_6, endpoint=company, order=4)
+        TestEndpoint.objects.create(test=test_6, endpoint=offers, order=5)
+        TestEndpoint.objects.create(test=test_6, endpoint=history, order=6)
 
         print("DONE")
